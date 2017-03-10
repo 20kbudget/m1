9242fe478f66a84da376796035cc6e29dd989dd7
Fabricio C Zuardi <fabricio@fabricio.org>
2017-03-10 17:58:22 -0300

    immutable as dependency, prettier and uglifyify on the build system

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // @flow
+const { Map } = require('immutable');
+
+const ui = Map({
+});
+
+const player = Map({
+});
+
+const background = Map({
+});
+
+const components = Map({
+    ui,
+    player,
+    background
+});
+
