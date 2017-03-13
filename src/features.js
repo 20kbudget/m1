68050f25e1ec861954e1a6736759417a305adc6a
Fabricio C Zuardi <fabricio@fabricio.org>
2017-03-13 00:28:13 -0300

    shape of pauseToggle components stable?

--- src/features.js
+++ src/features.js
@@ -5,46 +5,38 @@ type MessageThrough = {
     match: MatchingFunction,
     reply: ReplyFunction
 };
-// type State = Object;
-// type Reducer = (state: State) => State;
-// type TickReducer = ({ t: number, oldState: State, end: Reducer }) => State;
-// type AnimationMap = {
-    // enter: TickReducer,
-    // leave: TickReducer
-// };
+export type Texture = {
+    id?: string,
+    url?: string,
+    size?: number
+};
 
 const { Set, List, Map } = require('immutable');
+const uuid = require('uuid/v4');
 
-const boolProp = (name, defaultValue) =>
-    (value: boolean = defaultValue) => [name, value];
-const floatProp = (name, defaultValue) =>
-    (value: number = defaultValue) => [name, value];
 const listProp = (name, defaultValue) =>
     (value: Array<mixed> = defaultValue) => [name, List(value)];
-const messageThroughProp = (name, defaultValue: MessageThrough) =>
-    (value: MessageThrough = defaultValue) => [name, Map(value)];
-// const reducerMapProp = (name, defaultValue: AnimationMap) =>
-    // (value: AnimationMap = defaultValue) => [name, Map(value)];
 
-// Features (E-C-S components)
-const visibility = boolProp('visibility', false);
-const scale = floatProp('scale', 1.0);
+// Features (Components in the Entity-Components-System paradigm)
+const id = (value: string = uuid()) => ['id', value];
+const visibility = (value: boolean = false) => ['visibility', value];
 const position = listProp('position', [0, 0]);
 const sprites = listProp('sprites', []);
-const replies = messageThroughProp('replies', {
-    match: (msg, data) => false,
-    reply: (msg, data) => ''
-});
-// const animations = reducerMapProp('animations', {
-    // enter: ({ t, oldState, end }) => end(oldState),
-    // leave: ({ t, oldState, end }) => end(oldState)
-// });
+const texture = (t: Texture = {}) => ['texture', Map(t)];
+const replies = (
+    mt: MessageThrough = {
+        match: (msg, data) => false,
+        reply: (msg, data) => ''
+    }
+) => ['replies', mt];
+const update = (f: Function = ({ oldSelf }) => oldSelf) => ['update', f];
 
 module.exports = {
+    id,
     visibility,
-    scale,
     position,
     sprites,
+    texture,
+    update,
     replies
-    // animations
 };
--- src/pauseToggle.js
+++ src/pauseToggle.js
@@ -1,58 +1,42 @@
 // @flow
-type Texture = {
-    id: string,
-    url: string,
-    size: number
-};
+import type { Texture } from './features';
 type ToggleConstructorOptions = {
-    onIcon: Texture,
-    offIcon: Texture
+    value: boolean,
+    offIcon: Texture,
+    onIcon: Texture
 };
-type PauseToggle = (options: ToggleConstructorOptions) => Object;
+type Toggle = (options: ToggleConstructorOptions) => Object;
 
 const { Map } = require('immutable');
 const {
+    id,
     sprites,
-    visibility,
     position,
+    visibility,
+    texture,
     replies,
-    // animations
+    update
 } = require('./features');
 
-const pauseToggle:PauseToggle = ({ onIcon, offIcon }) =>
+const pauseToggleSprites = ({ value = false, offIcon, onIcon }) =>
+    sprites([
+        Map([texture(offIcon), visibility(!value)]),
+        Map([texture(onIcon), visibility(value)])
+    ]);
+
+const pauseToggle: Toggle = options =>
     Map([
-        ['id', 'pauseToggle'],
-        ['value', false],
-        visibility(),
+        id('pauseToggle'),
         position(),
-        sprites([
-            { texture: onIcon, visibility: true },
-            { texture: offIcon, visibility: false }
-        ]),
+        pauseToggleSprites(options),
         replies({
             match: msg => ['{id}:pointerdown', 'spacebar'].includes(msg),
             reply: (msg, data) => '{id}:clicked'
-        })
-        // animations({
-            // enter: ({ t, oldState, end }) =>
-                // end(oldState.merge({ visibility: true })),
-            // leave: ({ t, oldState, end }) =>
-                // end(oldState.merge({ visibility: false })),
-            // valueTrue: ({ t, oldState, end }) =>
-                // end(
-                    // oldState.merge({
-                        // iconsVisible: [false, true],
-                        // value: true
-                    // })
-                // ),
-            // valueFalse: ({ t, oldState, end }) =>
-                // end(
-                    // oldState.merge({
-                        // iconsVisible: [true, false],
-                        // value: false
-                    // })
-                // )
-        // })
+        }),
+        update(({ position, value, oldSelf }) =>
+            oldSelf.merge(
+                Map([position(position), pauseToggleSprites(options)])
+            ))
     ]);
 
 module.exports = pauseToggle;
